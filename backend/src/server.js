33f6451919dfa5952e5b02e9f2e4288f523457c8
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-05-09 21:26:14 +0530

    modify

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,38 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import mongoose from "mongoose";
 import { Server } from "socket.io";
 
 import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
-// DATABASE CONNECTION
-mongoose.connect(process.env.MONGO_URI)
+import connectDB from "./config/db.js";
 
-.then(() => {
-    console.log("MongoDB Connected");
-    const server = app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-    // SOCKET.IO SETUP
-    const io = new Server(server, {
-        cors: {
-            origin: "http://localhost:5173",
-            credentials: true
-        }
-    });
-    // Handle socket connections
-    io.on("connection", (socket) => {
-        console.log("User connected:", socket.id);
-        socket.on("disconnect", () => {
-            console.log("User disconnected:", socket.id);
-        });
-    });
-})
-.catch((err) => {
-    console.log(err);
+const PORT = process.env.PORT || 5000;
+
+
+// DATABASE CONNECTION
+connectDB();
+
+
+// EXPRESS SERVER
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running on port ${PORT}`
+  );
+});
+// SOCKET.IO
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true
+  }
+});
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+  socket.on("disconnect", () => {
+    console.log(
+      "User disconnected:",
+      socket.id
+    );
+  });
 });
