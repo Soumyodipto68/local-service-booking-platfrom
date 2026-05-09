2d1d43a20d4f6e3ae1952dc8724d338dc53c7fcc
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-05-09 21:15:34 +0530

    implementaion of socket io

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,33 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
+import { Server } from "socket.io";
+
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
-
+// DATABASE CONNECTION
 mongoose.connect(process.env.MONGO_URI)
+
 .then(() => {
     console.log("MongoDB Connected");
-
-    app.listen(PORT, () => {
+    const server = app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
-
+    // SOCKET.IO SETUP
+    const io = new Server(server, {
+        cors: {
+            origin: "http://localhost:5173",
+            credentials: true
+        }
+    });
+    // Handle socket connections
+    io.on("connection", (socket) => {
+        console.log("User connected:", socket.id);
+        socket.on("disconnect", () => {
+            console.log("User disconnected:", socket.id);
+        });
+    });
 })
 .catch((err) => {
     console.log(err);
