aef9d4885edb1c7b4104d41464c9653f793dc85e
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-05-10 13:36:55 +0530

    basic routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import CustomerDashboard from './pages/CustomerDashboard'
+import ProviderDashboard from './pages/ProviderDashboard'
+import AdminDashboard from './pages/AdminDashboard'
 const App = () => {
   return (
     <div>
       <Routes>
-        
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/customer-dashboard' element={<CustomerDashboard />} />
+        <Route path='/provider-dashboard' element={<ProviderDashboard />} />
+        <Route path='/admin-dashboard' element={<AdminDashboard />} />
       </Routes>
     </div>
   )
