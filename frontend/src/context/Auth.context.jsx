96f0afb808fa485f7a59e34d60f18d5857bdcf66
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-05-10 13:53:06 +0530

    auth context modified

--- frontend/src/context/Auth.context.jsx
+++ frontend/src/context/Auth.context.jsx
@@ -1,12 +1,43 @@
-import {createContext,useContext,useState} from "react";
+import {createContext, useContext, useEffect,useState} from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
-  const [user, setUser] = useState(null);
-   return (
-    <AuthContext.Provider value={{user,setUser}} >
-     {children}
+
+  const [user, setUser] =useState(null);
+
+  const [loading, setLoading] =useState(true);
+
+
+  // LOAD USER FROM LOCALSTORAGE
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+     if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+    setLoading(false);
+  }, []);
+
+
+  // LOGIN FUNCTION
+  const login = (userData) => {
+    localStorage.setItem("user",JSON.stringify(userData));
+    setUser(userData);
+  };
+
+
+  // LOGOUT FUNCTION
+  const logout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+
+  return (
+
+    <AuthContext.Provider value={{user,setUser,login,logout,loading}}>
+      {children}
     </AuthContext.Provider>
   );
 };
